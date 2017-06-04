4b0591b560e5c6ce245ca172215da5d3f0fe32b8
Hack Reactor Student <student@hackreactor.com>
2017-06-04 15:13:16 -0700

    Add init

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (  
+      <div>
+        'Hello World!'
+      </div>
+    );
+  }
+
+} 
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+window.App = App;
